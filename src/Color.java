7665588a165e10b1229b0cdd007b322e2f1b1acb
Dmitri Mandaji <badmopss@gmail.com>
2023-03-28 20:20:14 +0200

    add the enum Color

--- src/Color.java
+++ src/Color.java
@@ -1,7 +1,14 @@
 public enum Color {
-  WHITE,
-  BLACK,
-  GOLD,
-  GREY,
-  RED,
+  RED("Red"),
+  BLUE("Blue"),
+  GREEN("Green"),
+  YELLOW("Yellow"),
+  BLACK("Black"),
+  WHITE("White");
+
+  private final String name;
+
+  private Color(String name) {
+    this.name = name;
+  }
 }
