9c2a79882d577f29bb43969788777d574c98b4e3
Dmitri Mandaji <badmopss@gmail.com>
2023-03-28 21:01:16 +0200

    add the CarMain

--- src/CarMain.java
+++ src/CarMain.java
@@ -1,5 +1,21 @@
-public class CarMain {
-  public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class CarMain {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Выбрать цвет (Красный/Синий/Зеленый/Желтый/Черный): ");
+    String color = br.readLine();
+
+    System.out.println("Выбрать тип двигателя: ");
+    String engine = br.readLine();
+
+    System.out.println("Выбрать коробку передач: ");
+    String gear = br.readLine();
   }
 }
+ // Тип двигателя
+// Цвет
+// Коробка передач
