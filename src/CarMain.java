18551a31fd68fd450ac9b3104a649fba83763320
Dmitri Mandaji <badmopss@gmail.com>
2023-03-29 17:48:30 +0200

    add the LocalDate

--- src/CarMain.java
+++ src/CarMain.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 
 public class CarMain {
   public static void main(String[] args) throws IOException {
@@ -17,5 +18,7 @@ public class CarMain {
     }
     car.printCar();
     System.out.println("Стоимость выбранной вами комплектации = " + car.finalPrice());
+    LocalDate date = LocalDate.now();
+    System.out.println("Сегодня: " + date);
   }
 }
