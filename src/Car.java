09e586e6cd715be3adeaaedbcf9c2e2fe8e1874b
Eugen Borodin <Azsalatay@gmail.com>
2023-03-30 17:52:05 +0200

    Add Car.readCar

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Car {
   private CarModel model;
   private Color color;
@@ -50,28 +54,82 @@ public class Car {
   }
 
 
-  public void printCar(){
-    if (model != null){
+  public void printCar() {
+    if (model != null) {
       System.out.println("Модель вашей машины: " + model.carName);
     }
-    if (color != null){
+    if (color != null) {
       System.out.println("Цвет вашей машины: " + color.colorName);
     }
-    if(engine != null) {
+    if (engine != null) {
       System.out.println("Двигатель: " + engine.engineType);
     }
-    if(gear !=null) {
+    if (gear != null) {
       System.out.println("Коробка передач: " + gear.gearType);
     }
 
-    if(interior != null){
+    if (interior != null) {
       System.out.println("Салон Автомобиля:" + interior.interiorName);
     }
 
   }
-    public Integer finalPrice(){
-      return model.carPrice + color.colorPrice + engine.enginePrise + gear.gearPrice
-          + interior.interiorPrice;
-    }
+
+  public Integer finalPrice() {
+    return model.carPrice + color.colorPrice + engine.enginePrise + gear.gearPrice
+        + interior.interiorPrice;
   }
+
+  public static Car readCar() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Car car = new Car();
+    while (car.getModel() == null) {
+      try {
+        System.out.printf("Выберите Автомобиль своей мечты:%nBMW3-70000€%nBMW5-80000€%n" +
+            "BMW7-90000€%n");
+        car.setModel(CarModel.valueOf(br.readLine().toUpperCase()));
+      } catch (IllegalArgumentException e) {
+        System.out.println("Такого автомобиля нет в нашем салоне: " + e.getMessage());
+      }
+    }
+
+    while (car.getColor() == null) {
+      try {
+        System.out.printf("Выберите цвет вашей новой машины:%nRED-600€%nWHITE-500€%nBLACK-990€%n");
+        car.setColor(Color.valueOf(br.readLine().toUpperCase()));
+      } catch (IllegalArgumentException e) {
+        System.out.println("Такого цвета нет на складе, введите другой цвет: " + e.getMessage());
+      }
+    }
+
+    while (car.getEngine() == null) {
+      try {
+        System.out.printf("Выберете тип двигателя:%nDIESEL-10000€%nGASOLINE-12000%n");
+        car.setEngine(Engine.valueOf(br.readLine().toUpperCase()));
+      } catch (IllegalArgumentException e) {
+        System.out.println("Введите тип двигателя как указано в примере!!!" +
+            " (DIESEL / GASOLINE)" + e.getMessage());
+      }
+
+      while (car.getGear() == null) {
+        try {
+          System.out.printf("Выберите тип коробки передач:%nAUTO-9000€%nMANUEL-5000€%n");
+          car.setGear(Gear.valueOf(br.readLine().toUpperCase()));
+        } catch (IllegalArgumentException e) {
+          System.out.println("Такой коробки передач нет в нашем салоне: " + e.getMessage());
+        }
+      }
+      while (car.getInterior() == null) {
+        try {
+          System.out.printf("Выберите обшивку сидений:%nLEATHER-1000€%nALCANTARA-1400€%n" +
+              "TEXTILE-500€%n");
+          car.setInterior(Interior.valueOf(br.readLine().toUpperCase()));
+        } catch (IllegalArgumentException e) {
+          System.out.println("Такой коробки передач нет в нашем салоне: " + e.getMessage());
+        }
+      }
+    }
+    return car;
+  }
+}
+
 
--- src/CarMain.java
+++ src/CarMain.java
@@ -8,78 +8,32 @@ public class CarMain {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     System.out.println("ДОБРО ПОЖАЛОВАТЬ В АВТОСАЛОН ===ТРИ ТОВАРИЩА===");
 
-    Car car = new Car();
-    while (car.getModel() == null) {
-      try {
-        System.out.printf("Выберите Автомобиль своей мечты:%nBMW3-70000€%nBMW5-80000€%n" +
-            "BMW7-90000€%n");
-        car.setModel(CarModel.valueOf(br.readLine().toUpperCase()));
-      } catch (IllegalArgumentException e) {
-        System.out.println("Такого автомобиля нет в нашем салоне: " + e.getMessage());
-      }
-    }
+    Car car = Car.readCar();
+    car.printCar();
+    System.out.println("Стоимость выбранной вами комплектации = " + car.finalPrice() + " €");
 
-    while (car.getColor() == null) {
-      try {
-        System.out.printf("Выберите цвет вашей новой машины:%nRED-600€%nWHITE-500€%nBLACK-990€%n");
-        car.setColor(Color.valueOf(br.readLine().toUpperCase()));
-      } catch (IllegalArgumentException e) {
-        System.out.println("Такого цвета нет на складе, введите другой цвет: " + e.getMessage());
-      }
-    }
 
-    while (car.getEngine() == null) {
-      try {
-        System.out.printf("Выберете тип двигателя:%nDIESEL-10000€%nGASOLINE-12000%n");
-        car.setEngine(Engine.valueOf(br.readLine().toUpperCase()));
-      } catch (IllegalArgumentException e) {
-        System.out.println("Введите тип двигателя как указано в примере!!!" +
-            " (DIESEL / GASOLINE)" + e.getMessage());
+    System.out.printf("Выберете способ оплаты:%nНаличный расчет в нашем офисе -- Выберете: 1%n" +
+        "Безналичный расчет -- Выберете: 2%n");
+    try {
+      int payment = Integer.parseInt(br.readLine());
+      if (payment == 1) {
+        System.out.println("Будем рады вас видеть у нас в офисе в любой удобный для вас день");
+      } else {
+        System.out.println("Введите адрес EMAIL мы отправим Вам счет на оплату:");
       }
 
-      while (car.getGear() == null) {
-        try {
-          System.out.printf("Выберите тип коробки передач:%nAUTO-9000€%nMANUEL-5000€%n");
-          car.setGear(Gear.valueOf(br.readLine().toUpperCase()));
-        } catch (IllegalArgumentException e) {
-          System.out.println("Такой коробки передач нет в нашем салоне: " + e.getMessage());
-        }
+      String email = br.readLine();
+      if (!email.contains("@")) {
+        throw new InvalidEmail(email);
+      } else {
+        System.out.println("Счет на оплату был выслан на: " + email);
       }
-      while (car.getInterior() == null) {
-        try {
-          System.out.printf("Выберите обшивку сидений:%nLEATHER-1000€%nALCANTARA-1400€%n" +
-              "TEXTILE-500€%n");
-          car.setInterior(Interior.valueOf(br.readLine().toUpperCase()));
-        } catch (IllegalArgumentException e) {
-          System.out.println("Такой коробки передач нет в нашем салоне: " + e.getMessage());
-        }
-      }
-
-      car.printCar();
-      System.out.println("Стоимость выбранной вами комплектации = " + car.finalPrice() + " €");
-
-
-      System.out.printf("Выберете способ оплаты:%nНаличный расчет в нашем офисе -- Выберете: 1%n" +
-          "Безналичный расчет -- Выберете: 2%n");
-      try {
-        int payment = Integer.parseInt(br.readLine());
-        if (payment == 1) {
-          System.out.println("Будем рады вас видеть у нас в офисе в любой удобный для вас день");
-        } else {
-          System.out.println("Введите адрес EMAIL мы отправим Вам счет на оплату:");
-        }
-
-        String email = br.readLine();
-        if (!email.contains("@")) {
-          throw new InvalidEmail(email);
-        } else {
-          System.out.println("Счет на оплату был выслан на: " + email);
-        }
-        LocalDate date = LocalDate.now();
-        System.out.println("Поздравляем Вас вы заказали лучший автомобиль в нашем городе " + date);
-      } catch (NumberFormatException e) {
-        System.err.println("Некорректный ввод " + e.getMessage());
-      }
+      LocalDate date = LocalDate.now();
+      System.out.println("Поздравляем Вас вы заказали лучший автомобиль в нашем городе " + date);
+    } catch (NumberFormatException e) {
+      System.err.println("Некорректный ввод " + e.getMessage());
     }
   }
 }
+
