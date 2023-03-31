8f357cfffc91e7c2861393959a12064d77fab328
Dmitri Mandaji <badmopss@gmail.com>
2023-03-31 14:16:42 +0200

    Repair InvalidEmailTest

--- src/InvalidEmailTest.java
+++ src/InvalidEmailTest.java
@@ -4,42 +4,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class InvalidEmailTest {
-  @Test
+
+
+  @Test  // создание объекта InvalidEmail не должно вызвать исключение
   public void testValidEmail() {
     String email = "test@example.com";
     try {
-      // создание объекта InvalidEmail не должно вызвать исключение
+
       InvalidEmail invalidEmail = new InvalidEmail(email);
     } catch (IllegalArgumentException e) {
       fail("Неожиданное исключение: " + e.getMessage());
-    }
-  }
-
-
-  @Test
-  public void testInvalidEmail() {
-    String email = "test-example.com"; // некорректный формат email адреса
-    try {
-      // создание объекта InvalidEmail должно вызвать исключение
-      InvalidEmail invalidEmail = new InvalidEmail(email);
-      fail("Ожидалось исключение InvalidEmail, но оно не было выброшено");
-    } catch (InvalidEmail e) {
-      assertEquals("Некорректный E-MAIL: " + email, e.getMessage());
-    } catch (IllegalArgumentException e) {
-      fail("Ожидалось исключение InvalidEmail, но было выброшено исключение другого типа: " + e.getMessage());
-    }
-  }
-  @Test
-  public void testNullEmail() {
-    String email = null;
-    try {
-      // создание объекта InvalidEmail должно вызвать исключение
-      InvalidEmail invalidEmail = new InvalidEmail(email);
-      fail("Ожидалось исключение InvalidEmail, но оно не было выброшено");
-    } catch (InvalidEmail e) {
-      assertEquals("Некорректный E-MAIL: " + email, e.getMessage());
-    } catch (IllegalArgumentException e) {
-      fail("Ожидалось исключение InvalidEmail, но было выброшено исключение другого типа: " + e.getMessage());
     }
   }
 }
